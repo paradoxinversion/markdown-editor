78575270129278ca7bf81b2a1b6acb456460f87a
Jedai Saboteur <dev@paradoxinversion.com>
2017-11-27 13:13:37 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('layout'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
